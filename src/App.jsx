ee0108bdf093b79e7799d106141b5d93f7270dac
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-12-17 19:04:16 +0530

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function App() {
+  return <div>Hello Vite</div>;
+}
+
+// npm i eslint vite-plugin-eslint eslint-config-react-app --save-dev
+// packages to configure eslint in vite project.
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// npm i eslint vite-plugin-eslint eslint-config-react-app --save-dev
+// packages to configure eslint in vite project
